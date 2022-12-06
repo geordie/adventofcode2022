67a2958ca6e3687b993d3fd7f06a511d17f215bc
geordie <geordie.a.henderson@gmail.com>
2022-12-05 23:51:22 -0800

    Make day 6 code more compact

--- day6/signals.go
+++ day6/signals.go
@@ -11,14 +11,21 @@ import (
 type Buffer []rune
 
 func SolveDay6Puzzle2() {
+	solvePuzzle(14)
+}
 
+func SolveDay6Puzzle1() {
+	solvePuzzle(4)
+}
+
+func solvePuzzle(bufferSize int) {
 	file, err := os.Open("input/day6.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	reader := bufio.NewReader(file)
-	buffer := make(Buffer, 14)
+	buffer := make(Buffer, bufferSize)
 	i := 0
 
 	for {
@@ -35,39 +42,7 @@ func SolveDay6Puzzle2() {
 				return
 			}
 			// Shift the buffer
-			buffer = append(buffer[1:14], c)
-			i++
-		}
-	}
-}
-
-func SolveDay6Puzzle1() {
-
-	file, err := os.Open("input/day6.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	reader := bufio.NewReader(file)
-
-	buffer := make(Buffer, 4)
-	i := 0
-
-	for {
-		if c, _, err := reader.ReadRune(); err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				log.Fatal(err)
-			}
-		} else {
-			// If no duplicates were found, we have the answer
-			if !buffer.hasDups() {
-				fmt.Println("Day 6, PUZZLE 1 ANSWER: ", i)
-				return
-			}
-			// Shift the buffer
-			buffer = append(buffer[1:4], c)
+			buffer = append(buffer[1:bufferSize], c)
 			i++
 		}
 	}
